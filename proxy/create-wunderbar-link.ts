bc1bca0192651d7073abd1e12ba2b57a11073ce9
Tobias Viehweger <tobias.viehweger@yasoon.com>
2018-01-18 13:11:38 +0100

    update creation of navigation links to be in an own group (confluence calendars)

--- proxy/create-wunderbar-link.ts
+++ proxy/create-wunderbar-link.ts
@@ -5,10 +5,15 @@ import {
     FolderView, SearchFilter, FolderSchema, FindFoldersResults,
     ExtendedPropertyDefinition, MapiPropertyType,
     GetUserSettingsResponse, UserSettingName, PropertySet,
-    BasePropertySet, EmailMessage
+    BasePropertySet, EmailMessage, ItemView, ItemTraversal,
+    LogicalOperator,
+    IOutParam,
+    FindItemsResults,
+    Item
 } from "ews-javascript-api";
 import { applyCredentials, calculateStoreId, validateAutodiscoverRedirection } from "../proxy/helper";
 import { AutodiscoverService as NtlmAutodiscoverService } from '../extensions/CustomAutodiscoverService';
+import { Buffer } from "buffer";
 
 export class CreateWunderbarLinkRequest {
 
@@ -40,6 +45,9 @@ export class CreateWunderbarLinkRequest {
         let PidTagWlinkCalendarColor = new ExtendedPropertyDefinition(0x6853, MapiPropertyType.Integer);
         let PidTagWlinkROGroupType = new ExtendedPropertyDefinition(0x6892, MapiPropertyType.Integer);
         let PidTagWlinkAddressBookStoreEID = new ExtendedPropertyDefinition(0x6891, MapiPropertyType.Binary);
+        let PidTagWlinkGroupHeaderID = new ExtendedPropertyDefinition(0x6842, MapiPropertyType.Binary);
+        let PidTagWlinkSaveStamp = new ExtendedPropertyDefinition(0x6847, MapiPropertyType.Integer);
+        let PidTagWlinkOrdinal = new ExtendedPropertyDefinition(0x684B, MapiPropertyType.Binary);
 
         //Configure Autodiscover Service
         let autodiscoverService = new NtlmAutodiscoverService();
@@ -82,14 +90,85 @@ export class CreateWunderbarLinkRequest {
             objWunderBarLink.Subject = sharedCal.DisplayName;
         }
 
+        // 2B9065ED369E5C51BBDC2F7F8578942D => Confluence Calendars, GuidUtility.Create(GuidUtility.DnsNamespace, "com.yasoon.confluencecalendar.calendarGroup")
+        // B9F0060000000000C000000000000046 => Shared Calendars
+        // B8F0060000000000C000000000000046 => Other Calendars
+        // B7F0060000000000C000000000000046 => My Calendars
+        let confluenceGroupHeaderId = "2B9065ED369E5C51BBDC2F7F8578942D";
+        let knownGroups = [confluenceGroupHeaderId, "B9F0060000000000C000000000000046", "B8F0060000000000C000000000000046", "B7F0060000000000C000000000000046"]
+        let targetHeaderGuid = "B8F0060000000000C000000000000046";
+        let calendarFolderType = Buffer.from("0278060000000000C000000000000046", "hex");
+        let headers: FindItemsResults<Item>;
+        try {
+            //Find correct group header    
+            // => FolderTypes: https://msdn.microsoft.com/en-us/library/ee218711(v=exchg.80).aspx        
+            let associatedGroupHeaderView = new ItemView(100);
+            associatedGroupHeaderView.PropertySet = new PropertySet(BasePropertySet.FirstClassProperties, PidTagWlinkGroupHeaderID);
+            associatedGroupHeaderView.Traversal = ItemTraversal.Associated;
+            let groupHeaderSearchFilter = new SearchFilter.SearchFilterCollection(LogicalOperator.And);
+            groupHeaderSearchFilter.Add(new SearchFilter.IsEqualTo(new ExtendedPropertyDefinition(0x6849, MapiPropertyType.Integer), 4));
+            groupHeaderSearchFilter.Add(new SearchFilter.IsEqualTo(new ExtendedPropertyDefinition(0x684F, MapiPropertyType.Binary), calendarFolderType.toString('base64')));
+            headers = await commonViewsFolder.FindItems(groupHeaderSearchFilter, associatedGroupHeaderView);
+
+            let confluenceGroups = headers.Items.filter(i => {
+                let propValue: IOutParam<any> = <any>{};
+                if (i.ExtendedProperties.TryGetValue(PidTagWlinkGroupHeaderID, propValue)) {
+                    if (propValue.outValue)
+                        return new Buffer(propValue.outValue).toString('hex').toUpperCase() === confluenceGroupHeaderId;
+                }
+
+                return false;
+            });
+
+            if (confluenceGroups.length === 0) {
+                //According to https://msdn.microsoft.com/en-us/library/ee217241(v=exchg.80).aspx ([MS-OXOCFG] 4.4.1)
+                let newGroup = new EmailMessage(service);
+                newGroup.ItemClass = "IPM.Microsoft.WunderBar.Link";
+                newGroup.IsAssociated = true;
+                newGroup.Subject = "Confluence Calendars";
+                newGroup.SetExtendedProperty(PidTagWlinkGroupHeaderID, Buffer.from(confluenceGroupHeaderId, 'hex'));
+                newGroup.SetExtendedProperty(PidTagWlinkSaveStamp, Math.floor(Math.random() * 10000000));
+                newGroup.SetExtendedProperty(PidTagWlinkType, 4); //wblHeader
+                newGroup.SetExtendedProperty(PidTagWlinkFlags, 0);
+                newGroup.SetExtendedProperty(PidTagWlinkOrdinal, Buffer.from('80', 'hex'));
+                newGroup.SetExtendedProperty(PidTagWlinkFolderType, calendarFolderType);
+                newGroup.SetExtendedProperty(PidTagWlinkSection, 3);
+                await newGroup.Save(commonViewsFolder.Id);
+            }
+
+            targetHeaderGuid = confluenceGroupHeaderId;
+        }
+        catch (e) {
+            //Try falling back to known groups
+            try {
+                for (let i = 0; i < knownGroups.length; i++) {
+                    let matchedGroups = headers.Items.filter(item => {
+                        let propValue: IOutParam<any> = <any>{};
+                        if (item.ExtendedProperties.TryGetValue(PidTagWlinkGroupHeaderID, propValue)) {
+                            if (propValue.outValue)
+                                return new Buffer(propValue.outValue).toString('hex').toUpperCase() === knownGroups[i];
+                        }
+
+                        return false;
+                    });
+
+                    if (matchedGroups.length > 0) {
+                        targetHeaderGuid = knownGroups[i];
+                        break;
+                    }
+                }
+            }
+            catch (e) { }
+        }
+
         objWunderBarLink.ItemClass = "IPM.Microsoft.WunderBar.Link";
         //objWunderBarLink.SetExtendedProperty(PidTagWlinkAddressBookEID, Buffer.from(abTargetABEntryId, "hex"));
         objWunderBarLink.SetExtendedProperty(PidTagWlinkAddressBookStoreEID, Buffer.from(ownStoreId, "hex"));
         objWunderBarLink.SetExtendedProperty(PidTagWlinkCalendarColor, -1);
         objWunderBarLink.SetExtendedProperty(PidTagWlinkFlags, 0);
         objWunderBarLink.SetExtendedProperty(PidTagWlinkGroupName, "Shared Calendars");
-        objWunderBarLink.SetExtendedProperty(PidTagWlinkFolderType, Buffer.from("0278060000000000C000000000000046", "hex"));
-        objWunderBarLink.SetExtendedProperty(PidTagWlinkGroupClsid, Buffer.from("B9F0060000000000C000000000000046", "hex"));
+        objWunderBarLink.SetExtendedProperty(PidTagWlinkFolderType, calendarFolderType);
+        objWunderBarLink.SetExtendedProperty(PidTagWlinkGroupClsid, Buffer.from(targetHeaderGuid, "hex"));
         objWunderBarLink.SetExtendedProperty(PidTagWlinkROGroupType, -1);
         objWunderBarLink.SetExtendedProperty(PidTagWlinkSection, 3);
         objWunderBarLink.SetExtendedProperty(PidTagWlinkType, 2);
