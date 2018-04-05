32e0a48596a0d2c6e124ade72042a59d1bf39615
Tobias Viehweger <tobias.viehweger@yasoon.com>
2018-04-05 15:22:03 +0200

    some comments

--- proxy/update-event.ts
+++ proxy/update-event.ts
@@ -33,9 +33,12 @@ export class UpdateEventRequest {
         copyApiEventToAppointment(rawEvent, appointment);
 
         let mode: SendInvitationsOrCancellationsMode = SendInvitationsOrCancellationsMode.SendToNone;
+
+        //If attendees is set & only attendees are updated => Only send update to added attendees
         if (rawEvent.attendees && Object.keys(rawEvent).length === 1) {
             mode = SendInvitationsOrCancellationsMode.SendToChangedAndSaveCopy;
         } else if (rawEvent.attendees && rawEvent.attendees.length > 0 && Object.keys(rawEvent).length > 1) {
+            //Otherwise send update to all attendees
             mode = SendInvitationsOrCancellationsMode.SendToAllAndSaveCopy;
         }
 
