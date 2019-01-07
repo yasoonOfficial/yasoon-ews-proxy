374a0288ece86ff8a532c51ed03c41767eaa4f6b
Tobias Viehweger <tobias.viehweger@yasoon.com>
2019-01-07 13:30:33 +0100

    remove tag push from script

--- build/update-version.ps1
+++ build/update-version.ps1
@@ -3,5 +3,3 @@ $newVersion = (Get-Date).toString("yyyy-MM-dd@hh-mm")
 $a = Get-Content version.json -raw | ConvertFrom-Json
 $a.build = $newVersion
 $a | ConvertTo-Json  | set-content version.json
-git tag $a.build
-git push origin $a.build
