f95bf106272fb70768d4dfaba3910565a272c541
Tobias Viehweger <tobias.viehweger@yasoon.com>
2019-01-07 15:01:02 +0100

    fix version script

--- build/update-version.ps1
+++ build/update-version.ps1
@@ -1,5 +1,5 @@
 $newVersion = (Get-Date).toString("yyyy-MM-dd@hh-mm")
 
-$a = Get-Content version.json -raw | ConvertFrom-Json
+$a = Get-Content dist\version.json -raw | ConvertFrom-Json
 $a.build = $newVersion
-$a | ConvertTo-Json  | set-content version.json
+$a | ConvertTo-Json  | set-content dist\version.json
